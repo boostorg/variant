05f8bce381f91b7820b389d082aa3ce89d126eeb
Eric Friedman <ebf@users.sourceforge.net>
2003-10-19 02:35:04 +0000

    Initial release.
    [SVN r1682]

---
+++ test/type_switch_test.cpp
@@ -0,0 +1,69 @@
+#include "boost/test/minimal.hpp"
+#include "boost/type_switch.hpp"
+
+#include "boost/variant/variant.hpp"
+#include "boost/mpl/pair.hpp"
+#include "boost/mpl/placeholders.hpp"
+
+bool verify_pass_;
+
+template <bool B>
+struct verifier
+{
+    template <typename T>
+    void operator()(const T&) const
+    {
+        verify_pass_ = B;
+    }
+};
+
+typedef verifier<true> verify;
+typedef verifier<false> verify_not;
+
+#define VERIFY_LAST \
+    do { BOOST_CHECK( verify_pass_ ); verify_pass_ = false; } while(false)
+
+
+
+int test_main(int , char* [])
+{
+    namespace mpl = boost::mpl;
+    using namespace boost::type_switch;
+    using namespace boost::mpl::placeholders;
+
+    boost::variant<int, double> var1;
+
+    switch_(var1)
+      |= case_<int>( verify() )
+      |= case_<double>( verify_not() )
+      ;
+    VERIFY_LAST;
+
+    switch_(var1)
+      |= case_<float>( verify_not() )
+      |= default_( verify() )
+      ;
+    VERIFY_LAST;
+
+    switch_(var1)
+      |= case_< _ >( verify() )
+      ;
+    VERIFY_LAST;
+
+    boost::variant< mpl::pair<int,double>, int > var2;
+
+    switch_(var2)
+      |= case_< int >( verify_not() )
+      |= case_< mpl::pair<_,_> >( verify() )
+      ;
+    VERIFY_LAST;
+
+    switch_(var2)
+      |= case_< int >( verify_not() )
+      |= case_< mpl::pair<_1,_1> >( verify_not() )
+      |= case_< mpl::pair<_,_> >( verify() )
+      ;
+    VERIFY_LAST;
+
+    return boost::exit_success;
+}
