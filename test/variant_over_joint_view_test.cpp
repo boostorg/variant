caf6264c58aee177479243bdc2ea8800f1d5537c
Mike Maximoff <vigorous.activity@gmail.com>
2017-03-19 23:06:26 +0300

    Added test of related issue with mpl::set (trac #10041)

--- test/variant_over_joint_view_test.cpp
+++ test/variant_over_joint_view_test.cpp
@@ -16,13 +16,10 @@
 #include "boost/variant.hpp"
 #include "boost/mpl/joint_view.hpp"
 #include "boost/mpl/insert_range.hpp"
+#include "boost/mpl/set.hpp"
 
-typedef boost::variant<int> v1;
-typedef boost::variant<std::string> v2;
-typedef boost::make_variant_over<boost::mpl::joint_view<v1::types, v2::types>::type>::type v3;
-
-template<class T>
-void check_exception_on_get(v3& v) {
+template<class T, class Variant>
+void check_exception_on_get(Variant& v) {
     try {
         boost::get<T>(v);
         BOOST_FAIL("Expected exception boost::bad_get, but got nothing.");
@@ -30,8 +27,11 @@ void check_exception_on_get(v3& v) {
     } catch (...) { BOOST_FAIL("Expected exception boost::bad_get, but got something else."); }
 }
 
-int test_main(int , char* [])
-{
+void test_joint_view() {
+    typedef boost::variant<int> v1;
+    typedef boost::variant<std::string> v2;
+    typedef boost::make_variant_over<boost::mpl::joint_view<v1::types, v2::types>::type>::type v3;
+
     v1 a = 1;
     v2 b = "2";
     v3 c = a;
@@ -40,5 +40,23 @@ int test_main(int , char* [])
     BOOST_CHECK(boost::get<std::string>(d) == "2");
     check_exception_on_get<std::string>(c);
     check_exception_on_get<int>(d);
+}
+
+void test_set() {
+    typedef boost::mpl::set2< std::string, int > types;
+    typedef boost::make_variant_over< types >::type v;
+
+    v a = 1;
+    BOOST_CHECK(boost::get<int>(a) == 1);
+    check_exception_on_get<std::string>(a);
+    a = "2";
+    BOOST_CHECK(boost::get<std::string>(a) == "2");
+    check_exception_on_get<int>(a);
+}
+
+int test_main(int , char* [])
+{
+    test_joint_view();
+    test_set();
     return 0;
 }
