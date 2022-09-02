40621b12567497c2c7a1d77604d554ed9c914f01
Antony Polukhin <antoshkka@gmail.com>
2022-09-02 16:24:24 +0300

    fix c++03 compilation

--- test/recursive_variant_test.cpp
+++ test/recursive_variant_test.cpp
@@ -33,6 +33,8 @@
 #include <tuple>
 #endif // !defined(BOOST_VARIANT_DO_NOT_USE_VARIADIC_TEMPLATES) && !defined(BOOST_NO_CXX11_HDR_TUPLE)
 
+struct Nil {};
+
 struct printer
     : boost::static_visitor<std::string>
 {
@@ -358,10 +360,8 @@ void test_recursive_variant_over()
 void test_recursive_variant_from_variant()
 {
     // See https://github.com/boostorg/variant/issues/100
-
-    struct Nil {};
     typedef boost::variant<Nil, double> Atom;
-    typedef boost::variant<Nil, boost::recursive_wrapper<Atom>> Variant;
+    typedef boost::variant<Nil, boost::recursive_wrapper<Atom> > Variant;
 
     BOOST_STATIC_ASSERT(!boost::is_constructible<Variant, Atom>::value);
     BOOST_STATIC_ASSERT(boost::is_constructible<boost::variant<Nil, Atom>, Atom>::value);
